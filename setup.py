e167dad855eb85bab8f0731c04333c225216ae45
Jonas Tochtermann <thysia.zosa@gmail.com>
2021-05-31 21:33:37 +0200

    Add TomlFile for setub
    according to https://setuptools.readthedocs.io/en/latest/userguide/quickstart.html

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-with open("README", 'r') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
@@ -8,8 +8,8 @@ setup(
    version='1.0',
    description='A useful hardware monitoring tool',
    long_description=long_description,
-   author='Jonas Tochterman, Melvin Tas, Severin Hasler',
-   author_email='jonas.tochterman@edu.tbz.ch',
+   author='Jonas Tochtermann, Melvin Tas, Severin Hasler',
+   author_email='jonas.tochtermann@edu.tbz.ch',
    packages=['hardware-check'],  #internal packages | same as name
-   install_requires=['crontab', 'PyYAML', 'pyspectator', 'requests'], #external packages as dependencies
+   install_requires=['python-crontab', 'pyyaml', 'pyspectator', 'requests'], #external packages as dependencies
 )
